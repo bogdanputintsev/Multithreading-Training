e239ff4f685636f2931154c674e114aa9fcf9d88
Bogdan Putintsev <bogdan.putintsev@post.cz>
2022-11-28 23:43:33 +0100

    New changes, clean project

--- 01 - Pi Calculation Leibniz/01-pi-calculation-leibniz.cpp
+++ 01 - Pi Calculation Leibniz/01-pi-calculation-leibniz.cpp
@@ -1,50 +1,60 @@
 #include <cstdlib>
+#include <future>
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <cassert>
+#include <ctime>
 
-using namespace std;
+const int NUM_STEPS = 999999999; 
+const int NUM_THREADS = 8;
 
-#define NUM_STEPS 10000
-
-void calculateLeibnizPi(int start, int end, double& pi)
+namespace
 {
-	double result = 0;
-	for (int i = start; i < end; i++)
+	double calculateLeibnizPi(const int start, const int end)
 	{
-		result += 1.0 / (i * 4.0 + 1.0);
-		result -= 1.0 / (i * 4.0 + 3.0);
-	}
+		assert(start >= 0);
+		assert(end > start);
+		assert(end <= NUM_STEPS);
 
-	mutex m;
-	m.lock();
-	pi += result;
-	m.unlock();
+		double result = 0;
+		for (int i = start; i < end; i++)
+		{
+			result += 1.0 / (i * 4.0 + 1.0);
+			result -= 1.0 / (i * 4.0 + 3.0);
+		}
+
+		return result;
+	}
 }
+
 
 int main(int argc, char** argv)
 {
-	const int NUM_THREADS = 2;
+	const clock_t beginTime = clock();
 
 	double pi = 0.0;
-	vector<thread> threadContainer;
+	std::vector<std::future<double>> futureContainer;
 
 	for (int i = 0; i < NUM_THREADS; i++)
 	{
-		thread t(calculateLeibnizPi, i * (NUM_STEPS / NUM_THREADS),
-			(i + 1) * (NUM_STEPS / NUM_THREADS),
-			ref(pi));
-		threadContainer.push_back(move(t));
+		auto f = std::async(calculateLeibnizPi,
+			i * (NUM_STEPS / NUM_THREADS),
+			(i + 1) * (NUM_STEPS / NUM_THREADS));
+		futureContainer.push_back(std::move(f));
 	}
 
-
-	for (thread& th : threadContainer)
-		if (th.joinable())
-			th.join();
+	for (int i = 0; i < futureContainer.size(); i++)
+	{
+		pi += futureContainer[i].get();
+	}
 
 	pi *= 4.0;
-	printf("Pi done - %.40g \n", pi);
+	printf("PI calculation done - %.15g \n", pi);
 
-	return 0;
+	float timeElapsed = (float)(clock() - beginTime) / CLOCKS_PER_SEC;
+	printf("Execution time: %.3g\n", timeElapsed);
+
+	return EXIT_SUCCESS;
 }
 
